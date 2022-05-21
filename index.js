18e11fa15f8014103d5c540531952673653dea81
Naimur Rahib <61423411+7rahib@users.noreply.github.com>
2022-05-22 02:51:47 +0600

    all tools loading api created

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
+const app = express();
 const cors = require('cors');
 require('dotenv').config();
 
-const app = express();
+
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.y0nuc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
-        console.log('DB')
+        await client.connect()
+        const toolsCollection = client.db('toolsBuilder').collection('tools')
+
+        //Getting all tools data
+        app.get('/tools', async (req, res) => {
+            const query = {};
+            const cursor = toolsCollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools);
+        })
     }
     finally {
 
     }
 }
-
 run().catch(console.dir)
-
-app.use(cors());
-app.use(express.json());
 
 // ROOT API
 app.get('/', (req, res) => {
