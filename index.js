750013f048ee7309430a511a6eecf2bd14c6cbcb
Naimur Rahib <61423411+7rahib@users.noreply.github.com>
2022-05-21 23:54:52 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.y0nuc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        console.log('DB')
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
 
 app.use(cors());
 app.use(express.json());
